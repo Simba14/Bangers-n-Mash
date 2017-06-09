afc8719f0a0438a8cecd21c08bedba1f49021b0e
Sean Blundell <seanblundell@gmail.com>
2017-06-09 19:55:35 +0100

    Extract Api key into private directory

--- src/App.js
+++ src/App.js
@@ -2,8 +2,25 @@ import React, { Component } from 'react';
 import Header from './components/header';
 import VideoList from './components/video_list';
 import Video from './components/video';
+import axios from 'axios';
+import API_KEY from './private/api_key'
+
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videoIds: ['FSBB9uTfqp8'],
+      videos: 'p'
+    };
+  }
+
+  componentWillMount() {
+    axios.get(`https://www.googleapis.com/youtube/v3/videos?part=snippet,contentDetails&id=FSBB9uTfqp8&key=${API_KEY}`)
+      .then(response => this.setState({ videos: response.data.items }));
+  }
+
   render() {
     return (
       <div className="App">
@@ -14,6 +31,7 @@ class App extends Component {
         <div className='main-body'>
           <Video videoId='FSBB9uTfqp8' />
           <VideoList />
+          {console.log(this.state.videos)}
         </div>
       </div>
     );

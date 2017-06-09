079b7bbf375a9a726480df7c808947b658a2a032
Sean Blundell <seanblundell@gmail.com>
2017-06-09 17:01:18 +0100

    Create header component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Header from './components/header'
 
 class App extends Component {
   render() {
@@ -6,6 +7,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src="BR_Logo.jpg" className="App-logo" alt="logo" />
+          <Header>Boiler Room Picks</Header>
         </div>
       </div>
     );

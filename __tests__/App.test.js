546945b27fd840c0ec15b967cf354500a490be49
Sean Blundell <seanblundell@gmail.com>
2017-06-15 16:41:35 +0100

    Update App.test.js

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -4,6 +4,7 @@ import App from '../src/App';
 import { shallow } from 'enzyme';
 import Header from '../src/components/header';
 import VideoItem from '../src/components/video_item';
+import VideoList from '../src/components/video_item';
 
 test('renders without crashing', () => {
   const div = document.createElement('div');
@@ -18,4 +19,9 @@ test('renders a Header component', () => {
 test('renders a VideoItem component', () => {
   const wrapper = shallow(<App />);
   expect(wrapper.find(VideoItem).length).toBe(1);
+});
+
+test('renders two VideoList components', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.find(VideoList).length).toBe(1);
 });

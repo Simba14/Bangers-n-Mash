34da40638a8f578af9cd9b23245710af9760f214
Sean Blundell <seanblundell@gmail.com>
2017-06-15 16:37:30 +0100

    Add additional test to App.test

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -3,13 +3,19 @@ import ReactDOM from 'react-dom';
 import App from '../src/App';
 import { shallow } from 'enzyme';
 import Header from '../src/components/header';
+import VideoItem from '../src/components/video_item';
 
 test('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
 });
 
-test('renders one Text component', () => {
+test('renders a Header component', () => {
   const wrapper = shallow(<App />);
   expect(wrapper.find(Header).length).toBe(1);
+});
+
+test('renders a VideoItem component', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.find(VideoItem).length).toBe(1);
 });

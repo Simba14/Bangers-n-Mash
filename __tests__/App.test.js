0f54df6b32dfcab0dfab54a89c8b905764ee05ff
Sean Blundell <seanblundell@gmail.com>
2017-06-15 16:28:46 +0100

    Add snapshot test for App.js

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../src/App';
+import renderer from 'react-test-renderer';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+});
+
+it('renders App correctly', () => {
+  const tree = renderer.create(
+    <App />
+  ).toJSON();
+  expect(tree).toMatchSnapshot();
 });
